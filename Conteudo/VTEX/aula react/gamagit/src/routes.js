847f9a3fa7192b9808b056cd3d4f3a76f0daf359
Gisele Cortes <gisele.cortes@outlook.com>
2021-08-08 03:01:21 -0300

    Revert "limpando o arquivo"
    This reverts commit 4f0ddf1c6c5975c946472a4f95f8902212b1223e.

--- Conteudo/VTEX/aula react/gamagit/src/routes.js
+++ Conteudo/VTEX/aula react/gamagit/src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
-import repositories from './repositories';
-import Home from './Pages/Home';
+import repositores from './repositores';
+import Home from './home';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component= {Home} />
-                <Route path='/repositories' component={repositories} />
+                <Route path='/repositories' component={repositores} />
             </Switch>
         </BrowserRouter>
     )
