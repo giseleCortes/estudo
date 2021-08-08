b3fb60b62e48ecc80daf05060d2e835f448a84b2
Gisele Cortes <gisele.cortes@outlook.com>
2021-08-07 23:58:18 -0300

    Revert "Update routes.js"
    This reverts commit ec5da167b3636eb4d2e457ab140f2f738018d1a0.

--- Conteudo/VTEX/aula react/gamagit/src/routes.js
+++ Conteudo/VTEX/aula react/gamagit/src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
 import repositores from './repositores';
-import Home from './home';
+import Home from '.Home';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='/' exact component= {Home} />
+                <Route path='/' component= {Home} />
                 <Route path='/repositories' component={repositores} />
             </Switch>
         </BrowserRouter>
