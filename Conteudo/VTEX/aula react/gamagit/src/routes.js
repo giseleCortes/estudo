fed9427cea18d274e77b99bf4185c0d41d22fb17
Gisele Cortes <gisele.cortes@outlook.com>
2021-08-08 03:13:42 -0300

    limpando e estilizando

--- Conteudo/VTEX/aula react/gamagit/src/routes.js
+++ Conteudo/VTEX/aula react/gamagit/src/routes.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
-import repositores from './repositores';
-import Home from './home';
+import repositories from './pages/REPOSITORIOS';
+import Home from './pages/HOME';
+
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path='/' exact component= {Home} />
-                <Route path='/repositories' component={repositores} />
+                <Route path='/repositories' component={repositories} />
             </Switch>
         </BrowserRouter>
     )
