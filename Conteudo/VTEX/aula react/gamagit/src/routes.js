ec5da167b3636eb4d2e457ab140f2f738018d1a0
Gisele Cortes <gisele.cortes@outlook.com>
2021-08-07 23:45:00 -0300

    Update routes.js

--- Conteudo/VTEX/aula react/gamagit/src/routes.js
+++ Conteudo/VTEX/aula react/gamagit/src/routes.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route, BrowserRouter} from 'react-router-dom';
 import repositores from './repositores';
-import Home from '.Home';
+import Home from './home';
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path='/' component= {Home} />
+                <Route path='/' exact component= {Home} />
                 <Route path='/repositories' component={repositores} />
             </Switch>
         </BrowserRouter>
